b154c11e3fb126a8123e4807c98c5cab1b23618d
BuzzKill Master <BuzzKillMaster@proton.me>
2023-02-26 17:15:49 +0100

    move head to app file and remove unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,4 @@
-import Head from 'next/head'
 import React, {useEffect, useState} from "react";
-import ThemeToggle from "@/components/ThemeToggle";
-import SearchBar from "@/components/SearchBar";
-import InformationContainer from "@/components/InformationContainer";
 import AppContainer from "@/components/AppContainer";
 
 export default function Home() {
@@ -37,17 +33,8 @@ export default function Home() {
     }, [])
 
     return (
-        <>
-            <Head>
-                <title>DevFinder</title>
-                <meta name="description"
-                      content="DevFinder enables you to search for any developer's username on GitHub and get a quick summary of their profile."/>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main className={isDark ? "dark" : ""}>
-                {themeLocated && <AppContainer isDark={isDark} switchTheme={switchTheme}/>}
-            </main>
-        </>
+        <main className={isDark ? "dark" : ""}>
+            {themeLocated && <AppContainer isDark={isDark} switchTheme={switchTheme}/>}
+        </main>
     )
 }
